5b5c30af48e251c5c874ee0088265446e0dfdb16
Jake Nusca <mail@jakenusca.com>
2016-12-01 15:42:33 -0500

    Built basic structure. File path textbox used for navigating. Displays folder contents in list. List not interactive yet.

--- fbrowse.js
+++ fbrowse.js
@@ -5,6 +5,65 @@
 	filedata API and an event handler. Feel free to start fresh!
 */
 
+var CURRENT_PATH = "/";
+
+navigateToPath(CURRENT_PATH);
+
+
+function navigateToPath(path) {
+    filedata.getFilesForPath(path, function(err, files)
+    {
+        console.log(err);
+        console.log(files);
+        console.log(files[0]);
+        console.log(files[0].name);
+        if(err)
+        {
+            $("#log-output").text("ERROR: " + JSON.stringify(err));
+        }
+        else
+        {
+            buildFileList(files);
+            $("#path-box").val(path);
+        }
+    });
+}
+
+function buildFileList(files) {
+    var filesUl = $('<ul/>');
+    for(i = 0; i < files.length; i++) {
+        var file = files[i];
+        var fileLi = $('<li/>');
+
+        var link = $("<a/>");
+
+        console.log(file);
+        console.log(file.name);
+        link.text(file.name);
+
+        fileLi.append(link);
+        filesUl.append(fileLi);
+    };
+    $("#files-window").empty();
+    $("#files-window").append(filesUl);
+}
+
+$("#go-button").on('click', function() {
+    var path = $("#path-box").val();
+    console.log(path);
+    navigateToPath(path);
+});
+
+
+
+
+
+
+
+
+
+
+
 $("button").on("click", function()
 {
     filedata.getFilesForPath("/", function(err, files)
